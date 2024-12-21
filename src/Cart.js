061c0b997ad9b4f6a861fa0fb56c2bef0827d956
TadaaLenard <lenard8794@gmail.com>
2024-12-21 22:20:56 +0800

    cart rectangles

---
+++ src/Cart.js
@@ -0,0 +1,19 @@
+import './Cart.css';
+import React from 'react';
+import Menubar from './Component/Menubar';
+
+function Cart() {
+  return (
+    <>
+    <Menubar />
+    <div className='ItemList'>
+        <p>Hi</p>
+    </div>
+    <div className='Summary'>
+        <p>Hi</p>
+    </div>
+    </>
+  );
+}
+
+export default Cart;
---
+++ src/Component/Menubar.js
@@ -0,0 +1,9 @@
+import './Menubar.css';
+
+function Menubar(){
+    return(
+        <p>This is a menu</p>
+    );
+}
+
+export default Menubar;
