f0c32a79e99053d20d6e3380d4867399e023d290
TadaaLenard <lenard8794@gmail.com>
2024-12-23 23:02:19 +0800

    Cart card tick option and item photo

--- src/Component/Cartlist.js
+++ src/Component/Cartlist.js
@@ -1,7 +1,36 @@
+import React, { useState } from "react";
 import './Cartlist.css';
 
-function Cartlist(){
 
+function Cartlist(){
+    //setStatus function for button
+    const [SelectedStatus, setStatus] = useState("NotSelected");
+
+    const changeStatus = () => {
+        if (SelectedStatus !== "NotSelected") {
+            setStatus("NotSelected");
+            console.log("Not selected");
+        }
+        else {
+            setStatus("Selected");
+            console.log("Changed state");
+        }
+    };
+    
+    return (
+        <div className='CartCard'>
+            <button 
+                className={SelectedStatus}
+                onClick={changeStatus}>
+                ✓
+            </button>
+            <div className="ItemPhoto">
+            <img src={"/Images/cart.jpg"} alt="" />
+            </div>
+            <div>Item details</div>
+            <div>Price</div>
+        </div>
+    )
 }
 
 export default Cartlist;
