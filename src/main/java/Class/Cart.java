6e506616164d3fb842836dd320334d218f29f27b
TadaaLenard <lenard8794@gmail.com>
2025-01-10 20:42:06 +0800

    Change cart.csv to resources, use CSVreader

--- src/main/java/Class/Cart.java
+++ src/main/java/Class/Cart.java
@@ -4,20 +4,29 @@ import java.util.ArrayList;
 
 public class Cart {
     private int client_id;
-    private ArrayList<Integer> product_list = new ArrayList<>();
+    private ArrayList<String> product_list = new ArrayList<>();
     private ArrayList<Integer> quantity_list = new ArrayList<>();
 
     //Constructor
+    public Cart() {
+
+    }
+
     public Cart(int client_id) {
         this.client_id = client_id;
     }
 
-    //Getter
+    public void setCart(Cart cart) {
+        this.client_id = cart.client_id;
+        this.product_list = cart.product_list;
+        this.quantity_list = cart.quantity_list;
+    }
+
     public int getClient_id() {
         return client_id;
     }
 
-    public void getPro_Quantity(int product_id) {
+    public void getPro_Quantity(String product_id) {
         for (int i = 0; i < product_list.size(); i++) {
             if (product_list.get(i) == product_id) {
                 System.out.println("Product_id: " + product_list.get(i));
@@ -30,7 +39,7 @@ public class Cart {
     }
 
     //Add to cart
-    public void addCart(int product_id, int quantity) {
+    public void addCart(String product_id, int quantity) {
         if(product_list.size() == 0){
             product_list.add(product_id);
             quantity_list.add(quantity);
@@ -52,7 +61,7 @@ public class Cart {
     }
 
     //Remove from cart
-    public void removeCart(int product_id) {
+    public void removeCart(String product_id) {
         for(int i = 0; i < product_list.size(); i++)
         {
             product_list.remove(i);
@@ -61,7 +70,7 @@ public class Cart {
     }
 
     //Subtract from cart
-    public void subtractCart(int product_id, int quantity) {
+    public void subtractCart(String product_id, int quantity) {
         for (int i = 0; i < product_list.size(); i++) {
             if (product_list.get(i) == product_id) {
                 quantity_list.set(i, quantity_list.get(i) - quantity);
@@ -72,11 +81,11 @@ public class Cart {
         }
     }
 
-    public ArrayList<Integer> getProduct_id(){
+    public ArrayList<String> getProduct_id(){
         return product_list;
     }
 
-    public int getProduct_id(int index){
+    public String getProduct_id(int index){
         return product_list.get(index);
     }
 
--- src/main/java/Class/Global.java
+++ src/main/java/Class/Global.java
@@ -1,12 +1,18 @@
 package Class;
 
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Global {
     private static ArrayList<Product> ProductList = new ArrayList<Product>();
-    private static final String FILE_PATH = "C:\\Users\\60115\\IdeaProjects\\CAT201-Project\\src\\main\\resources\\catProjectDataset.csv";
+    private static final String FILE_PATH = Global.class.getClassLoader().getResource("catProjectDataset.csv").getPath();
 
     public static ArrayList<Product> getProductList() {
 
@@ -53,4 +59,75 @@ public class Global {
         }
         return discount;
     }
-}
+
+    //Currently using bufferedreader from java.io library
+    //Load Cart.csv that contains all the clients' cart data
+    public static void loadCart(ArrayList<Product> products,
+                                ArrayList<Cart> carts,
+                                Cart client_cart,
+                                ArrayList<Product> cart_products,
+                                int client_id) throws IOException, CsvValidationException {
+        CSVReader reader;
+        try {
+            reader = new CSVReader(
+                    new FileReader(
+                            Global.class.getClassLoader().getResource("Cart.csv").getPath()
+                    )
+            );
+            System.out.println("File found"); //Found
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        carts.clear();
+        String[] line;
+        reader.readNext();
+        System.out.println("Reader read");
+
+        while ((line = reader.readNext()) != null) {
+            System.out.println(Arrays.toString(line));
+            if (line.length == 3) {
+                int client = Integer.parseInt(line[0].trim());
+                String sku = line[1].trim();
+                int quantity = Integer.parseInt(line[2].trim());
+
+                boolean found = false;
+                for (int i = 0; i < carts.size(); i++) {
+                    if (carts.get(i).getClient_id() == client) {
+                        carts.get(i).addCart(sku, quantity);
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    carts.add(new Cart(client));
+                    carts.get(carts.size() - 1).addCart(sku, quantity);
+                }
+                System.out.println(sku + "" + quantity);
+            }
+        }
+            System.out.println("End of file reached.");
+
+            for (int i = 0; i < carts.size() - 1; i++) {
+                if (carts.get(i).getClient_id() == client_id) {
+                    client_cart.setCart(carts.get(i));
+                    System.out.println("client_cart added." + client_cart.getProduct_id());
+                    break;
+                }
+            }
+
+            //for each cart product in the list
+            for (int i = 0; i < client_cart.getProduct_id().size(); i++) {
+                //Search for the respective products in the products
+                System.out.println("client_product list looped." + client_cart.getProduct_id(i));
+                for (int k = 0; k < products.size(); k++) {
+                    System.out.println("product list looped." + products.get(k).getProduct_sku());
+                    if (client_cart.getProduct_id(i).equals(products.get(k).getProduct_sku())) {
+                        cart_products.add(products.get(k));
+                        System.out.println("client_product list added." + cart_products);
+                        break;
+                    }
+                }
+            }
+        System.out.println("client_cart list shown." + client_cart);
+        }
+    }
